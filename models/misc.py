c5e5c95deda980062fafc51c72c23be835a7a7f4
ZorEl212 <yabsirad212@gmail.com>
2024-12-22 07:52:42 -0500

    models/misc: Port methods from legacy bot [1/?]
    Port some general purpose methods in to `Utilities` class
    
    keeping them as class methods or static methods for now
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/misc.py
+++ models/misc.py
@@ -1,5 +1,14 @@
 import asyncio
 import random
+import aiohttp
+import subprocess
+
+from resources.constants import (
+    ADJECTIVES,
+    ADMIN_ID,
+    NOUNS,
+    TIME_ZONE,
+)
 
 class Auth:
     # --- Authorization ---
@@ -18,3 +27,75 @@ class Auth:
             return await func(self, event, *args, **kwargs)
 
         return wrapper
+
+
+class Utilities:
+    @staticmethod
+    def get_day_suffix(day):
+        if 11 <= day <= 13:
+            return "th"
+        return {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
+
+    @staticmethod
+    def generate_password():
+        return (
+                random.choice(ADJECTIVES)
+                + random.choice(NOUNS)
+                + "".join(random.choices(string.digits, k=4))
+        )
+
+    @staticmethod
+    def parse_duration(duration_str: str):
+        duration_str = duration_str.lower()
+        total_seconds = 0
+        current_number = ""
+        for char in duration_str:
+            if char.isdigit():
+                current_number += char
+            else:
+                if char == "d":
+                    total_seconds += int(current_number) * 24 * 60 * 60
+                elif char == "h":
+                    total_seconds += int(current_number) * 60 * 60
+                elif char == "m":
+                    total_seconds += int(current_number) * 60
+                elif char == "s":
+                    total_seconds += int(current_number)
+                current_number = ""
+        return total_seconds
+
+    @staticmethod
+    def get_date_str(epoch: int):
+        ist = pytz.timezone(TIME_ZONE)
+        date = datetime.datetime.fromtimestamp(epoch, ist)
+        day_suffix = Utilities.get_day_suffix(date.day)
+        day = date.day
+        return date.strftime(f"{day}{day_suffix} %B %Y, %I:%M %p IST")
+
+    @classmethod
+    def parse_duration_to_human_readable(cls, duration_seconds: int) -> str:
+        if duration_seconds <= 0:
+            return "Expired"
+        duration_str = ""
+        if duration_seconds // (24 * 3600) > 0:
+            duration_str += f"{duration_seconds // (24 * 3600)} days, "
+            duration_seconds %= 24 * 3600
+        if duration_seconds // 3600 > 0:
+            duration_str += f"{duration_seconds // 3600} hours, "
+            duration_seconds %= 3600
+        if duration_seconds // 60 > 0:
+            duration_str += f"{duration_seconds // 60} minutes, "
+            duration_seconds %= 60
+        if duration_seconds > 0:
+            duration_str += f"{duration_seconds} seconds"
+        return duration_str
+
+    @classmethod
+    async def get_exchange_rate(cls, from_currency, to_currency):
+        url = f"https://api.exchangerate-api.com/v4/latest/{from_currency}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                return data["rates"][to_currency]
+
+
