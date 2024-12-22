647a3a95c8c6cd889bb3ac813db1472d8bd8eb3f
ZorEl212 <yabsirad212@gmail.com>
2024-12-22 08:04:55 -0500

    models/engine: DB: implement a method for querying all instances of a Class
    This can be useful in scenarios where we need to query every row from a table with or without filters. even for cases where collecting every row from every table is required.
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/engine/db_engine.py
+++ models/engine/db_engine.py
@@ -15,6 +15,31 @@ class DBStorage:
     def __init__(self):
         self.__engine = create_engine("sqlite:///server-database.db", echo=False)
 
+    def all(self, cls=None, filters=None):
+        """Query objects from the database with optional class and filters."""
+        new_dict = {}
+
+        if cls:
+            target_class = classes.get(cls) if isinstance(cls, str) else cls
+            if target_class not in classes.values():
+                raise ValueError(f"Class '{cls}' not found.")
+
+            query = self.__session.query(target_class)
+            objs = query.filter_by(**filters).all() if filters else query.all()
+
+            for obj in objs:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                new_dict[key] = obj
+            return new_dict
+
+        for clss_name, clss in classes.items():
+            objs = self.__session.query(clss).all()
+            for obj in objs:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                new_dict[key] = obj
+
+        return new_dict
+
     def reload(self):
         Base.metadata.create_all(self.__engine)
         ses_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
