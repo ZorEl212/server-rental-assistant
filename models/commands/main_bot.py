a159f395c33875cb8802ebdc99943eca77146727
ZorEl212 <yabsirad212@gmail.com>
2024-12-22 08:35:39 -0500

    models/commands: Define core bot management class
    Here we handle all the defined handlers and bot initialization.
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

---
+++ models/commands/main_bot.py
@@ -0,0 +1,30 @@
+from telethon import TelegramClient, events
+import typing
+from typing import Dict
+
+from resources.constants import (API_ID, API_HASH, BOT_TOKEN)
+
+
+class BotManager:
+    __client: TelegramClient = None
+    def __init__(self, routes, client=None):
+        self.__client = TelegramClient("server_plan_bot", API_ID, API_HASH) if not client else client
+        self.ROUTES: Dict = routes
+
+    def start(self):
+        self.__client.start(bot_token=BOT_TOKEN)
+        self.__client.add_event_handler(self.command_handler, events.NewMessage(pattern="/"))
+        print("Bot is running...")
+        self.__client.run_until_disconnected()
+
+    @property
+    def client(self):
+        return self.__client
+
+    async def command_handler(self, event):
+        command = event.message.text.split()[0]
+        handler = self.ROUTES.get(command)
+        if handler:
+            await handler(event)
+        else:
+            await event.respond("Unknown command. Type /help for available commands.")
