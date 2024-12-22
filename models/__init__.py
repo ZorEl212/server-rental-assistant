5d59c6d06378e914a6b954fa04b3e20f385ad9ca
ZorEl212 <yabsirad212@gmail.com>
2024-12-22 08:37:30 -0500

    models/init: Initialize `BotManager` and map routes to handlers
    teleton doesn't have native mapping support so we gonna have to take care of it by ourselves
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,38 @@
 from models.engine.db_engine import DBStorage
+from telethon import TelegramClient, events
 from resources.constants import API_ID, API_HASH
 
 storage = DBStorage()
 storage.reload()
 
+client = TelegramClient("server_plan_bot", API_ID, API_HASH)
+
+from models.commands.user import UserRoutes
+from models.commands.rental import PlanRoutes
+from models.commands.payment import PaymentRoutes
+from models.commands.system import SystemRoutes
+from models.commands.main_bot import BotManager
+
+
+user_routes = UserRoutes()
+plan_routes = PlanRoutes()
+payment_routes = PaymentRoutes()
+system_routes = SystemRoutes()
+
+routes = {
+    '/start': system_routes.start_command,
+    '/help': system_routes.help_command,
+    '/reduce_plan': plan_routes.reduce_plan,
+    '/extend_plan': plan_routes.extend_plan,
+    '/create_user': user_routes.create_user,
+    '/delete_user': user_routes.delete_user_command,
+    '/list_users': user_routes.list_users,
+    '/payment_history': payment_routes.payment_history,
+    '/gen_report': system_routes.generate_report,
+    '/broadcast': system_routes.broadcast,
+    'unlink_user': user_routes.clear_user,
+    '/link_user': user_routes.link_user,
+    'who': system_routes.list_connected_users
+}
+
+bot = BotManager(client=client, routes=routes)
