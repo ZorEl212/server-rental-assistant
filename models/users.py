8d34b561776b9391b1b204a8ed403ef7b0c97060
ZorEl212 <yabsirad212@gmail.com>
2024-12-23 01:39:35 -0500

    models/user: Prepare `Users`table for `credit` and `debit` commands

--- models/users.py
+++ models/users.py
@@ -10,6 +10,7 @@ class User(BaseModel, Base):
     uuid = Column(Text, unique=True, default=None)
     linux_username = Column(Text, unique=True, nullable=False)
     linux_password = Column(Text, nullable=False)
+    balance = Column(Integer, default=0)
 
     # Relationships
     telegram_user = relationship(
@@ -17,3 +18,19 @@ class User(BaseModel, Base):
     )
     rentals = relationship("Rental", back_populates="user", cascade="all, delete")
     payments = relationship("Payment", back_populates="user", cascade="all, delete")
+
+    async def update_balance(self, amount, transaction_type):
+        """
+        Record a transaction, updating the user's balance.
+        :param amount: Amount to credit or debit.
+        :param transaction_type: 'payment' or 'refund'.
+        """
+
+        if transaction_type == "credit":
+            self.balance += amount
+        elif transaction_type == "debit":
+            if amount > self.balance:
+                raise ValueError("Insufficient balance.")
+            self.balance -= amount
+        else:
+            raise ValueError("Invalid transaction type.")
