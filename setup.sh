b946c05c3f5efd7867b188aa77579d12ad6265d9
Shravan Vadeghar <namanageshwar@outlook.com>
2025-01-24 19:04:32 +0000

    setup.sh: Add setup script for environment and Redis installation

---
+++ setup.sh
@@ -0,0 +1,42 @@
+# Check if python is installed
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 is not installed. Please install python3"
+    exit 1
+fi
+
+# Check if pip is installed
+if ! command -v pip3 &> /dev/null
+then
+    echo "Pip3 is not installed. Please install pip3"
+    exit 1
+fi
+
+# Check if virtualenv is installed
+if ! command -v virtualenv &> /dev/null
+then
+    echo "Virtualenv is not installed. Please install virtualenv"
+    exit 1
+fi
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages
+pip3 install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
+# Install redis server
+sudo apt-get install lsb-release curl gpg
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis -y
+
+echo -e "\e[32mSetup complete. Run 'source venv/bin/activate' to activate the virtual environment\e[0m"
